26355f9c9b52a40688f6bc4e39f8c969c319a438
ibrahem abodeif <ibrahem.mohamed191999@gmail.com>
2025-11-23 20:07:00 +0200

    update dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DatabaseModule } from './Core/database/db.module';
+import { PlaceModule } from './Modules/place/place.module';
+import { PlaceTypeModule } from './Modules/place-type/place-type.module';
+import { FacelitiesModule } from './Modules/facelities/facelities.module';
+import { CloudinaryModule } from './Core/cloudinary/cloudinary.module';
+
+import { LoggerModule } from './Core/filters/logger.module';
+import { UserModule } from './Modules/user/user.module';
+import { AuthModule } from './Modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    CloudinaryModule,
+    LoggerModule,
+    PlaceModule,
+    PlaceTypeModule,
+    FacelitiesModule,
+    UserModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ResponseInterceptor } from './Core/interceptors/response.interceptor';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+      transform: true,
+    }),
+  );
+
+  app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
